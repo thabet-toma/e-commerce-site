a3c14f3e0d6b0ae2969757052762aeb8b56e8221
thabet toma <thapet64@gmail.com>
2022-10-11 16:23:06 +0300

    user session

--- app1/models.py
+++ app1/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+import re
+
+class locationManager(models.Manager):
+    def basic_validator(self,postData):
+        errors={}
+        if len(postData['region']) < 3:
+            errors["region"] = "region  should be at least 2 characters"
+        if len(postData['building']) < 3:
+            errors["building"] = "building should be at least 2 characters"
+        if len(postData['street']) < 3:
+            errors["street"] = "street should be at least 2 characters"
+        if len(postData['apartment']) < 0:
+            errors["apartment"] = "apartment required"
+    
+
+        
+
+        return errors
 
 
 class location(models.Model):
@@ -10,11 +28,12 @@ class location(models.Model):
         apartment=models.CharField(max_length=255)
         created_at=models.DateTimeField(auto_now_add=True)
         updated_at=models.DateTimeField(auto_now=True)
-import re
+        objects=locationManager()
+
 class UserManager(models.Manager):
     def basic_validator(self,postData):
         errors={}
-        if len(postData['first_name']) < 3:
+        if len(postData['first_name']) <3:
             errors["first_name"] = "First name should be at least 2 characters"
         if len(postData['last_name']) < 3:
             errors["last_name"] = "Last name should be at least 2 characters"
@@ -31,8 +50,10 @@ class UserManager(models.Manager):
 
         if len(postData['password']) < 9:
             errors["password"] = "password should be at least 8 characters"
-        if not postData['password']==postData['confirm']:
-            errors["password"]='invalid password'
+    
+        phone_regex=re.compile(r'^[0-9]')
+        # if not email_regex.match(postData['phone_number']):    # test whether a field matches the pattern            
+        #     errors['phone_number'] = "Invalid phone number"
 
         return errors
 
@@ -42,10 +63,11 @@ class Users(models.Model):
     last_name=models.CharField(max_length=255)
     email=models.CharField(max_length=255)
     password=models.CharField(max_length=255)
-    phone_number=models.CharField(max_length=14,blank=True)
+    phone_number=models.CharField(max_length=14,blank=True,null=True)
     location=models.ForeignKey(location,related_name="user_loc",on_delete=None)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    objects=UserManager()
 class categories(models.Model):
 
     category=models.CharField(max_length=255)
@@ -57,12 +79,12 @@ class products(models.Model):
     model_y=models.IntegerField()
     description=models.TextField()
     price=models.IntegerField()
-    categotry=models.ForeignKey(categories,related_name='product',on_delete=None ,blank=True)
+    categotry=models.ForeignKey(categories,related_name='product',on_delete=None ,blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 class orders(models.Model):
 
-    location=models.ForeignKey(location,related_name="order_loc",on_delete=None)
+
     user=models.ForeignKey(Users,related_name="user_ord",on_delete=None)
     product=models.ManyToManyField(products,related_name="order_product")
 
